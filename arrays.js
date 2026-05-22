2c6a4f07e5b2c0e391144cd5e7e34b7ac83ffb8e
bellyfisher <527icubley@frhsd.com>
2026-05-22 08:33:43 -0400

    javascript

---
+++ arrays.js
@@ -0,0 +1,4 @@
+let forwards = ['Barzal', 'Lee', 'Pageau', 'Holmstrom', 'Duclair', 'Palat', 'Palmieri', 'Cizikas', 'Gatcomb', 'Heineman', 'MacLean', 'Schenn']
+let defense = ['Schaefer', 'Pulock', 'Pelech', 'Mayfield', 'Romanov', 'Boqvist', 'DeAngelo', 'Soucy', 'Dobson']
+let goalies = ['Sorokin', 'Varlamov', 'Rittich', 'Smith']
+let retired = ['Potvin', 'Bossy', 'Gillies', 'Trottier', 'Nystrom', 'Tonelli', 'Smith', 'Goring']

e7d2ce312f300dc4c90c82fb6d62b06b4fd32bef
bellyfisher <527icubley@frhsd.com>
2026-05-22 09:13:04 -0400

    i hate modals

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-let forwards = ['Barzal', 'Lee', 'Pageau', 'Holmstrom', 'Duclair', 'Palat', 'Palmieri', 'Cizikas', 'Gatcomb', 'Heineman', 'MacLean', 'Schenn']
-let defense = ['Schaefer', 'Pulock', 'Pelech', 'Mayfield', 'Romanov', 'Boqvist', 'DeAngelo', 'Soucy', 'Dobson']
+let forwards = ['Barzal', 'Lee', 'Pageau', 'Holmstrom', 'Duclair', 'Palat', 'Palmieri', 'Cizikas', 'Gatcomb', 'Heineman', 'MacLean', 'Schenn', 'Gillies', 'Trottier', 'Bossy', 'Nystrom', 'Tonelli', 'Goring']
+let defense = ['Schaefer', 'Pulock', 'Pelech', 'Mayfield', 'Romanov', 'Boqvist', 'DeAngelo', 'Soucy', 'Dobson', 'Potvin']
 let goalies = ['Sorokin', 'Varlamov', 'Rittich', 'Smith']
 let retired = ['Potvin', 'Bossy', 'Gillies', 'Trottier', 'Nystrom', 'Tonelli', 'Smith', 'Goring']
